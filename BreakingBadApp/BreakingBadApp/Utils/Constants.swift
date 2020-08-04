4f537e48e9ae9b1f5bf09f73263e11816d3b79f1
Alexander Tsvetanov <alexandervltsvetanov@gmail.com>
2020-08-04 17:21:15 +0300

    Networking implemented

---
+++ BreakingBadApp/BreakingBadApp/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  BreakingBadApp
+//
+//  Created by Sasha Belov on 4.08.20.
+//  Copyright © 2020 Alexander Tsvetanov. All rights reserved.
+//
+
+import Foundation
+
+struct Urls {
+    static let getCharactersUrl = "https://breakingbadapi.com/api/characters"
+}
